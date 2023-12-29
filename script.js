f2a0a80fa1cad54cc28c29f01b8dff5a37367130
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2023-12-29 16:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+let year = prompt( );
+
+if(year%4 === 0)
+{
+	if(year%100 === 0){
+		if(year%400 === 0)
+		{
+			alert("366");
+		}
+		else{
+			alert("365");
+		}
+	}
+}
+else
+{
+	alert("365");
+}
+
+
+
+
